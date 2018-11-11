874cec30d038a00ec82b773f19cf8f9b53554f69
rolandn <roland.neyrinck@gmail.com>
2018-11-11 19:56:38 +0100

    Premier commit principal

---
+++ CD_facture_template.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Agile Business Group sagl
+# (<http://www.agilebg.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Roland NEYRINCK
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Facture custom pour Casa Domotic",
+    'version': '10.0.1',
+    'category': 'Generic Modules/Accounting',
+    'author':   "Roland NEYRINCK",
+    'website': '',
+    'license': 'AGPL-3',
+    "depends": [
+        'account',
+    ],
+    "data": [
+        'Facture_CD.xml',
+    ],
+    'installable': True,
+}
