3defdbc2a4df08e0106c22d42721ca70b8afd9f6
rolandn <roland.neyrinck@gmail.com>
2019-01-16 18:53:09 +0100

    ajouter date limite de paiement dans la vue

--- CD_facture_template.py
+++ CD_facture_template.py
@@ -10,3 +10,11 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     duedate = fields.Date(string="Date limite de payement", required=False, track_visibility='onchange')
+    message_id = fields.Many2one('message.invoice', string='Message', store=True, required=False, track_visibility='onchange', help="Remarque sur le taux de TVA appliqué. Ex : Autoliquidation de la TVA.")
+
+class Message(models.Model):
+    _name = 'message.invoice'
+
+    name = fields.Char(string="Text du message", required=True)
+    active = fields.Boolean('Actif ?', default=True)
+
