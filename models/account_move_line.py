2ad25424d6a2f48a25393cd5514bdbfcbea64d4f
Roland Neyrinck <neyrinckr>
2021-01-11 14:42:07 +0100

    Ajout du champ active dans account_move_line.py

--- models/account_move_line.py
+++ models/account_move_line.py
@@ -1,7 +1,7 @@
 from odoo import fields, models, api
 
 
-class AccountMoveLineInherit(models.Model):
+class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     active = fields.Boolean("Actif")
