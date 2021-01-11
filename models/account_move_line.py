4cdbe5b35b41ad7de9e0155336f1a2ebf5da9c0e
Roland Neyrinck <neyrinckr>
2021-01-11 15:02:09 +0100

    Ajout du champ active dans account_move_line.py

--- models/account_move_line.py
+++ models/account_move_line.py
@@ -1,6 +1,4 @@
-
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -10,4 +8,4 @@ class AccountMoveLine(models.Model):
 
     _inherit = 'account.move.line'
 
-#    active = fields.Boolean(string="Actif", default=True)
+    active = fields.Boolean(string="Actif", default=True)
