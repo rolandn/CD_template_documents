c941db1283e93089aac15f28ddc71f07e1a81b8e
Roland Neyrinck <neyrinckr>
2021-01-11 14:48:57 +0100

    Ajout du champ active dans account_move_line.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+
 from . import CD_facture_template
 from . import escompte
 from . import account_move_line
